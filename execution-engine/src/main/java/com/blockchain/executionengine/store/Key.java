6a0b814d5655642f3a2931d2f5aaead1aac7905c
Bala Siva Sai Akhil Malepati <malepatia@vmware.com>
2021-01-13 14:42:17 -0800

    Add new Key Class files for EE and SKVBC

---
+++ execution-engine/src/main/java/com/blockchain/executionengine/store/Key.java
@@ -0,0 +1,11 @@
+package com.blockchain.executionengine.store;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Key {
+    private String key;
+}
---
+++ simplekvbc/src/main/java/com/blockchain/simplekvbc/store/Key.java
@@ -0,0 +1,12 @@
+package com.blockchain.simplekvbc.store;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Key {
+    private String key;
+}
